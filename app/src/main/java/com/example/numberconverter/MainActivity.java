3c767996b8aafdd6f20897712b5da72b77c73a5e
dhirender04 <kumardhirender04@gmail.com>
2020-10-12 23:29:01 +0530

    completed the all types of numbers like hundred thousand etc dought in lakh or crore is right or write million billion ,, modify the layout also .

--- app/src/main/java/com/example/numberconverter/MainActivity.java
+++ app/src/main/java/com/example/numberconverter/MainActivity.java
@@ -11,54 +11,64 @@ import android.widget.TextView;
 import java.sql.Struct;
 
 public class MainActivity extends AppCompatActivity {
-//    private EditText enterNumber;
-//    private Button convert;
-//    private TextView txtView;
+    private EditText enterNumber;
+    private Button convert;
+    private TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        enterNumber=findViewById(R.id.enterNumber);
-//        txtView=findViewById(R.id.txtView);
-//        convert=findViewById(R.id.btnConvert);
+        enterNumber=findViewById(R.id.enterNumber);
+        txtView=findViewById(R.id.txtView);
+        convert=findViewById(R.id.btnConvert);
 
-//        convert.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                int n1=Integer.parseInt(String.valueOf(enterNumber.getText()));
-//                String word=convertNumber(n1);
-//                txtView.setText(word);
-//
-//            }
-//        });
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int n1=Integer.parseInt(String.valueOf(enterNumber.getText()));
+                String word=convertNumber(n1);
+                txtView.setText(word);
+
+            }
+        });
 
 
     }
-//    String fun(int n){
-//        String str="";
-//        String[] units={"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine",
-//        "Ten","Eleven","Twelve","Thirteen","Fourteen",
-//                "Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
-//
-//        String[] tens={"","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
-//
-//        //Code for Converting Numbers to Words//
-//
-//        if(n<20) {
-//            str = units[n];
-//        }
-//            else if(n>=20 && n<100 ){
-//                str=tens[n/10]+units[n%10];
-//            }
-//
-//        return str;
-//    }
-//    String convertNumber(int num){
-//        String str1="";
-//        if(num<100){
-//            str1=fun(num);
-//        }
-//        return str1;
-//    }
+    String fun(int n){
+        String str="";
+        String[] units={""," One "," Two "," Three "," Four "," Five "," Six "," Seven ",
+                " Eight "," Nine ", " Ten "," Eleven "," Twelve "," Thirteen "," Fourteen ",
+                " Fifteen "," Sixteen "," Seventeen "," Eighteen "," Nineteen "};
+
+        String[] tens={"",""," Twenty "," Thirty "," Forty "," Fifty "," Sixty ",
+                " Seventy "," Eighty ", " Ninety "};
+
+        //Code for Converting Numbers to Words//
+
+        if(n<20) {
+            str = units[n];
+        }
+            else if(n>=20 && n<100 ){
+                str=tens[n/10]+units[n%10];
+            }
+
+        return str;
+    }
+    String convertNumber(int num){
+        String str1="";
+        if(num<100){
+            str1=fun(num);
+        }else if(num>=100 && num<1000){
+            str1=fun(num/100)+" Hundred " +convertNumber(num%100);
+        }else  if(num>=1000 && num<100000){
+            str1=fun(num/1000)+ " Thousand "+convertNumber(num%1000);
+        }else  if(num>=100000 && num<10000000){
+            str1=fun(num/100000)+" Lakh "+convertNumber(num%100000);
+        }
+        else if (num>10000000 && num<1000000000){
+            str1=fun(num/10000000)+ " Crore " +convertNumber(num%10000000);
+        }
+        return str1;
+    }
 }
